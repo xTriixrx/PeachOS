151f874ad8a607816e6232109487fb08a0ddb06b
xTriixrx <vnigro5743@gmail.com>
2024-02-11 16:14:32 -0700

    initial elf loader implementation

---
+++ include/loader/formats/elfloader.h
@@ -0,0 +1,43 @@
+#ifndef ELFLOADER_H
+#define ELFLOADER_H
+
+#include "elf.h"
+#include "config.h"
+#include <stdint.h>
+#include <stddef.h>
+
+struct elf_file
+{
+    char filename[MAX_PATH];
+    int in_memory_size;
+    
+    /**
+     * The physical memory address that this elf file is loaded at
+     */
+    void* elf_memory;
+
+    /**
+     * The virtual base address of this binary
+     */
+    void* virtual_base_address;
+
+    /**
+     * The ending virtual address this binary
+     */
+    void* virtual_end_address;
+
+    /**
+     * The physical base address of this binary
+     */
+    void* physical_base_address;
+
+    /**
+     * The physical end address of this binary
+     */
+    void* physical_end_address;
+};
+
+int elf_close(struct elf_file*);
+int elf_load(const char*, struct elf_file**);
+
+#endif
