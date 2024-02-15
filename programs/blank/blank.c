f3ca0104d558067e308982ed1033e8e048331a33
xTriixrx <vnigro5743@gmail.com>
2024-02-14 18:15:40 -0700

    stdlib print implementation

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,5 +1,12 @@
+#include "stdlib.h"
 
 int main(int argc, char** argv)
 {
+    print("Hello World!\n");
+
+    while(1) {
+
+    }
+    
     return 0;
 }
---
+++ programs/stdlib/src/stdlib.asm
@@ -0,0 +1,13 @@
+[BITS 32]
+
+global print:function
+
+print:
+    push ebp
+    mov ebp, esp
+    push dword[ebp + 8]
+    mov eax, 1 ; Command print
+    int 0x80
+    add esp, 4
+    pop ebp
+    ret
